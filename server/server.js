41c06fd2228f4b3d888298700d22d828e28b98d4
Alan-Miller <alan.ashman@gmail.com>
2017-11-21 23:04:20 -0700

    testing headers

--- server/server.js
+++ server/server.js
@@ -15,18 +15,18 @@ massive(CONNECTION_STRING).then(db => {
 app.use(express.static(`${__dirname}/../build`));
 app.use((req, res, next) => { console.log(req.method, req.url); next(); });
 
-app.use((req, res, next) => {
-    res.status(200).set({
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-        'X-XSS-Protection': '1; mode=block',
-        'X-Frame-Options': 'SAMEORIGIN'
-        // 'Content-Security-Policy': "default-src 'self' devmountain.github.io"
-    });
-    next();
-});
+// app.use((req, res, next) => {
+//     res.status(200).set({
+//         'Content-Type': 'application/json',
+//         'Access-Control-Allow-Origin': '*',
+//         'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+//         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+//         'X-XSS-Protection': '1; mode=block',
+//         'X-Frame-Options': 'SAMEORIGIN'
+//         // 'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+//     });
+//     next();
+// });
 
 api(app);
 
