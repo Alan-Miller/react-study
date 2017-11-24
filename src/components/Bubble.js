f17f5026845c46b27d94d5001b8db28e575a5c59
Alan-Miller <alan.ashman@gmail.com>
2017-11-24 01:38:33 -0700

    working Bubble component

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import Button from './Button';
 
-export default function Bubble(props) {
+export default class Bubble extends React.Component {
 
-    return (
-        <div className="Bubble">
-            <div className="outerBox">
-                <div className="innerBox">
-                    <Button>Click</Button>
+    constructor(props) {
+        super(props)
+        this.state = {
+            showModal: false
+        }
+    }
+
+    fade() {
+
+    }
+
+    render() {
+        return (
+            <div className="Bubble">
+                <div className="outerBox"
+                    onClick={e => { console.log('OUTER'); this.setState({ showModal: false }); }}>
+                    <div
+                        className={this.state.showModal ? "modal showModal" : "modal"}
+                        onClick={e => { e.stopPropagation(); console.log('MODAL'); }}
+                    >{
+                        this.state.showModal ?
+                        <span className="x" onClick={() => this.setState({ showModal: false })}>x</span>
+                        : null
+                    }</div>
+                    <Button
+                        onClick={
+                            e => {
+                                console.log('BUTTON');
+                                e.stopPropagation();
+                                this.setState({ showModal: true });
+                            }}
+                    >Open Modal</Button>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Button(props) {
 
     return (
-        <div className="Button">
+        <div onClick={props.onClick} className={props.className || "Button"}>
             {props.children}
         </div>
     )
